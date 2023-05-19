0baf0930d9774f202ee6a90b5aa36668f18d4f6e
Idris Noor <idreesnoor150@gmail.com>
2023-05-19 09:31:49 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+
+
+# anagram.rb
+class Anagram
+    attr_reader :word
+  
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(words)
+      words.select { |w| anagram?(w) }
+    end
+  
+    private
+  
+    def anagram?(other_word)
+      sorted_word == sort_letters(other_word)
+    end
+  
+    def sorted_word
+      sort_letters(word)
+    end
+  
+    def sort_letters(string)
+      string.downcase.chars.sort.join
+    end
+  end
+  
